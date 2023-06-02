f95783c6c26c06ff5b143726ea117ab632c16560
Gerardo Aguilar <gerardo.aguilar01@outlook.com>
2023-06-01 18:58:14 -0600

    Preserved state with useRef

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import './App.css'
 import { UsersList } from './components/UsersList'
 import { type User } from './types'
@@ -7,6 +7,7 @@ function App() {
   const [users, setUsers] = useState<User[]>([])
   const [showColor, setShowColor] = useState<boolean>(false)
   const [sort, setSort] = useState<boolean>(false)
+  const originalUsers = useRef<User[]>([])
 
   const toogleColor = () => setShowColor(!showColor)
 
@@ -17,11 +18,14 @@ function App() {
     setUsers(filteredUsers)
   }
 
+  const handleReset = () => setUsers(originalUsers.current)
+
   useEffect(() => {
     fetch('https://randomuser.me/api/?results=100')
       .then(async r => await r.json())
       .then(res => {
         setUsers(res.results)
+        originalUsers.current = res.results
       })
       .catch(err =>
         console.log(err))
@@ -39,6 +43,7 @@ function App() {
       <header>
         <button onClick={toogleColor}>Color Rows</button>
         <button onClick={toogleSort}>{sort ? 'Do not sort by Country' : 'Sort by Country'}</button>
+        <button onClick={handleReset}>Reset State</button>
       </header>
       <main>
         <UsersList deleteUser={handleDelete} showColor={showColor} users={sortedUsers} />
